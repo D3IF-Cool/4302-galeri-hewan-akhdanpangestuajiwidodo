e5f6449285226aef847f3d440ed44b8960a78a88
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-05-06 09:55:14 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if0008/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if0008/galerihewan/model/Hewan.kt
@@ -1,8 +1,9 @@
 package org.d3if0008.galerihewan.model
 
+import org.d3if0008.galerihewan.R
+
 data class Hewan (
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int,
-    val jenis: String
+    val imageResId: Int = R.drawable.angsa
 )
--- app/src/main/java/org/d3if0008/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if0008/galerihewan/ui/main/MainViewModel.kt
@@ -15,23 +15,7 @@ class MainViewModel: ViewModel(){
     private val data = MutableLiveData<List<Hewan>>()
 
     init {
-        data.value = initData()
         retrieveData()
-    }
-
-    private fun initData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", R.drawable.angsa, "(Unggas)"),
-            Hewan("Ayam", "Gallus gallus", R.drawable.ayam, "(Unggas)"),
-            Hewan("Bebek", "Cairina moschatta", R.drawable.bebek, "(Unggas)"),
-            Hewan("Domba", "Ovis ammon", R.drawable.domba, "(Mamalia)"),
-            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun, "(Unggas)"),
-            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing, "(Mamalia)"),
-            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci, "(Mamalia)"),
-            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau, "(Mamalia)"),
-            Hewan("Kuda", "Equus caballus", R.drawable.kuda, "(Mamalia)"),
-            Hewan("Sapi", "Bos taurus", R.drawable.sapi, "(Mamalia)")
-        )
     }
 
     fun getData(): LiveData<List<Hewan>> = data
@@ -39,8 +23,7 @@ class MainViewModel: ViewModel(){
     private fun retrieveData(){
         viewModelScope.launch {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             }catch (e: Exception){
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
